8abab2a1c358362d5e92422997848d439dcd46db
NgocTrungTran <milotran1496@gmail.com>
2022-08-05 11:04:55 +0700

    TH Customer thymeleaf

---
+++ Thymeleaf/CustomerThymeleaf/src/main/java/com/milo/customerthymeleaf/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.milo.customerthymeleaf.service;
+
+import com.milo.customerthymeleaf.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
